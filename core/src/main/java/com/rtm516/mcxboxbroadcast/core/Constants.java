93b7b69af4372227fe11f48ed2bc849395723ea7
Elias <99972450+elias1731@users.noreply.github.com>
2025-05-26 01:10:29 +0200

    Constants.java aktualisieren

--- core/src/main/java/com/rtm516/mcxboxbroadcast/core/Constants.java
+++ core/src/main/java/com/rtm516/mcxboxbroadcast/core/Constants.java
@@ -5,86 +5,58 @@ import com.google.gson.GsonBuilder;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
 import org.cloudburstmc.protocol.bedrock.codec.v800.Bedrock_v800;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.URI;
 import java.time.Instant;
 import java.util.Date;
-import java.util.Properties;
 
 public class Constants {
     public static final Gson GSON = new GsonBuilder()
-            .registerTypeAdapter(Instant.class, new InstantConverter())
-            .registerTypeAdapter(Date.class, new DateConverter())
-            .disableHtmlEscaping()
-            .create();
+        .registerTypeAdapter(Instant.class, new InstantConverter())
+        .registerTypeAdapter(Date.class, new DateConverter())
+        .disableHtmlEscaping()
+        .create();
 
     public static final Gson GSON_NULLS = new GsonBuilder()
-            .registerTypeAdapter(Instant.class, new InstantConverter())
-            .registerTypeAdapter(Date.class, new DateConverter())
-            .serializeNulls()
-            .create();
+        .registerTypeAdapter(Instant.class, new InstantConverter())
+        .registerTypeAdapter(Date.class, new DateConverter())
+        .serializeNulls()
+        .create();
 
-    // Werte aus Konfig
-    public static final String SERVICE_CONFIG_ID;
-    public static final String TEMPLATE_NAME;
-    public static final String TITLE_ID;
+    public static final String SERVICE_CONFIG_ID = "4fc10100-5f7a-4470-899b-280835760c07"; // The service config ID for Minecraft
+    public static final String TEMPLATE_NAME = "MinecraftLobby";
+    public static final String TITLE_ID = "896928775"; // The title ID for Minecraft Windows Edition
+    public static final String CREATE_SESSION = "https://sessiondirectory.xboxlive.com/serviceconfigs/" + SERVICE_CONFIG_ID + "/sessionTemplates/" + TEMPLATE_NAME + "/sessions/%s";
+    public static final String JOIN_SESSION = "https://sessiondirectory.xboxlive.com/handles/%s/session";
 
-    // Endpunkte
-    public static final String CREATE_SESSION;
-    public static final String JOIN_SESSION;
+    public static final String PLAYFAB_LOGIN = "https://20ca2.playfabapi.com/Client/LoginWithXbox";
+    public static final URI START_SESSION = URI.create("https://authorization.franchise.minecraft-services.net/api/v1.0/session/start");
+    public static final String RTC_WEBSOCKET_FORMAT = "wss://signal.franchise.minecraft-services.net/ws/v1.0/signaling/%s";
 
-    public static final String PLAYFAB_LOGIN;
-    public static final URI START_SESSION;
-    public static final String RTC_WEBSOCKET_FORMAT;
+    public static final URI RTA_WEBSOCKET = URI.create("wss://rta.xboxlive.com/connect");
+    public static final URI CREATE_HANDLE = URI.create("https://sessiondirectory.xboxlive.com/handles");
 
-    public static final URI RTA_WEBSOCKET;
-    public static final URI CREATE_HANDLE;
+    public static final String PEOPLE = "https://social.xboxlive.com/users/me/people/xuid(%s)";
+    public static final String USER_PRESENCE = "https://userpresence.xboxlive.com/users/xuid(%s)/devices/current/titles/current";
+    public static final URI FOLLOWERS = URI.create("https://peoplehub.xboxlive.com/users/me/people/followers");
+    public static final URI SOCIAL = URI.create("https://peoplehub.xboxlive.com/users/me/people/social");
+    public static final URI SOCIAL_SUMMARY = URI.create("https://social.xboxlive.com/users/me/summary");
+    public static final String FOLLOWER = "https://social.xboxlive.com/users/me/people/follower/xuid(%s)";
+    public static final String PROFILE_SETTINGS = "https://profile.xboxlive.com/users/xuid(%s)/profile/settings?settings=Gamertag";
 
-    public static final String PEOPLE;
-    public static final String USER_PRESENCE;
-    public static final URI FOLLOWERS;
-    public static final URI SOCIAL;
-    public static final URI SOCIAL_SUMMARY;
-    public static final String FOLLOWER;
-    public static final String PROFILE_SETTINGS;
+    public static final String GALLERY = "https://persona.franchise.minecraft-services.net/api/v1.0/gallery";
 
-    public static final String GALLERY;
-
+    /**
+     * Gathered from scraped web requests
+     */
     public static final int ConnectionTypeWebRTC = 3;
+
+    /**
+     * Used to be 1000, but the limit was increased in Aug 2024
+     */
     public static final int MAX_FRIENDS = 2000;
+
+    /**
+     * Used for the micro nethernet server that trasnfers the client to the real server
+     */
     public static final BedrockCodec BEDROCK_CODEC = Bedrock_v800.CODEC;
-
-    static {
-        Properties props = new Properties();
-        try {
-            props.load(new FileInputStream("config.properties"));
-        } catch (IOException e) {
-            System.err.println("⚠️ Konnte config.properties nicht laden – Standardwerte werden genutzt.");
-        }
-
-        SERVICE_CONFIG_ID = props.getProperty("service_config_id", "4fc10100-5f7a-4470-899b-280835760c07");
-        TEMPLATE_NAME     = props.getProperty("template_name", "MinecraftLobby");
-        TITLE_ID          = props.getProperty("title_id", "896928775");
-
-        CREATE_SESSION = "https://sessiondirectory.xboxlive.com/serviceconfigs/" + SERVICE_CONFIG_ID + "/sessionTemplates/" + TEMPLATE_NAME + "/sessions/%s";
-        JOIN_SESSION   = "https://sessiondirectory.xboxlive.com/handles/%s/session";
-
-        PLAYFAB_LOGIN         = props.getProperty("playfab_login", "https://20ca2.playfabapi.com/Client/LoginWithXbox");
-        START_SESSION         = URI.create(props.getProperty("start_session", "https://authorization.franchise.minecraft-services.net/api/v1.0/session/start"));
-        RTC_WEBSOCKET_FORMAT  = props.getProperty("rtc_ws", "wss://signal.franchise.minecraft-services.net/ws/v1.0/signaling/%s");
-
-        RTA_WEBSOCKET = URI.create("wss://rta.xboxlive.com/connect");
-        CREATE_HANDLE = URI.create("https://sessiondirectory.xboxlive.com/handles");
-
-        PEOPLE           = "https://social.xboxlive.com/users/me/people/xuid(%s)";
-        USER_PRESENCE    = "https://userpresence.xboxlive.com/users/xuid(%s)/devices/current/titles/current";
-        FOLLOWERS        = URI.create("https://peoplehub.xboxlive.com/users/me/people/followers");
-        SOCIAL           = URI.create("https://peoplehub.xboxlive.com/users/me/people/social");
-        SOCIAL_SUMMARY   = URI.create("https://social.xboxlive.com/users/me/summary");
-        FOLLOWER         = "https://social.xboxlive.com/users/me/people/follower/xuid(%s)";
-        PROFILE_SETTINGS = "https://profile.xboxlive.com/users/xuid(%s)/profile/settings?settings=Gamertag";
-
-        GALLERY = "https://persona.franchise.minecraft-services.net/api/v1.0/gallery";
-    }
 }
